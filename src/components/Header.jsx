8b3e4ef8b834f3b7037830ecb49ad436c2374e7a
Jazab Ahmad <jazabshoukat72@gmail.com>
2024-06-27 04:36:35 -0700

    update dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 import '../components/card.css'
 
-export default function Header( {isDark,setIsDark}) {
-    
-  
-    return (
-  
-      <>
-          <header className={isDark ? 'navbar darkElement': 'navbar'}>
+export default function Header({ isDark, setIsDark }) {
+
+
+  return (
+
+    <>
+      <header className={isDark ? 'navbar darkElement' : 'navbar'}>
         <div className="nav">
-            <h2>Where in the world?</h2>
-            <h5 onClick={()=>{ setIsDark(!isDark)}} > <i className="fa-regular fa-moon"></i>Dark Mode</h5>
+          <h2>Where in the world?</h2>
+          <h5 onClick={() => { setIsDark(!isDark) }} > <i className={isDark ? "fa-regular fa-sun" : "fa-regular fa-moon"}></i>{isDark ? "Light Mode" : "Dark Mode"}</h5>
         </div>
-    </header>
-      </>
-    )
-  }
+      </header>
+    </>
+  )
+}
